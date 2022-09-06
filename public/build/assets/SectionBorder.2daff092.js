c042434e0030995a046e656ea48fa5503de6a281
paulauqui <paul.codemotion@gmail.com>
2022-09-06 17:32:52 -0500

    Init

---
+++ public/build/assets/SectionBorder.2daff092.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./plugin-vue_export-helper.f63bf49c.js";import{o as s,e as c,a as e}from"./app.222e0c47.js";const r={},t={class:"hidden sm:block"},a=e("div",{class:"py-8"},[e("div",{class:"border-t border-gray-200"})],-1),n=[a];function _(d,i){return s(),c("div",t,n)}const f=o(r,[["render",_]]);export{f as J};
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [\App\Http\Controllers\FrontEnd\PageController::class,'home'])->name('frontend.home');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});

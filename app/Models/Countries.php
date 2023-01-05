3c7eb122be916fad3c2856016c73872f580170c8
paulauqui <paul.codemotion@codemotion.com.ec>
2023-01-05 19:26:57 +0000

    Filter country status

--- app/Models/Countries.php
+++ app/Models/Countries.php
@@ -4,10 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 
 class Countries extends Model
 {
     use HasFactory;
 
-    protected $table='countries';
+    protected $table = 'countries';
+
+    /**
+     * @param Builder $query
+     * @return mixed
+     */
+    public function scopeActive($query)
+    {
+        $query->select('countries.*')
+            ->where('status', 1);
+
+        return $query;
+    }
 }
